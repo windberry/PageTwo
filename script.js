5602c3cbd0b3ad3873ce5936db67039607be9ead
Lina155 <AlinfDEmaGitHub2021>
2021-03-03 14:00:41 +0200

    TwoFetch

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-fetch('api.openweathermap.org/data/2.5/weather?id=2172797&appid=b124ec8d5546495f87d1cfc27038a5cc')
-    .then(res => res.json())
-    .then(data =>
-    document.getElementById('root').innerHTML=
-    JSON.stringify(data)
+// async function getResponse(){
+//     let response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Vinnytsia&appid=b124ec8d5546495f87d1cfc27038a5cc')
+//     let content = await response.json()
+//     content = content.splice(0,10)
+//
+//     let key;
+//
+//     for(key in content){
+//         console.log(content[key])
+//     }
+// }
+// getResponse()
+
+ fetch(`https://api.openweathermap.org/data/2.5/weather?q=Vinnytsia&appid=b124ec8d5546495f87d1cfc27038a5cc`)
+    .then(weather=>{
+        console.log(weather);
+        return weather.json();
+    })
+    .then(
+        (weather) => {
+            console.log(weather);
+            printWeather(weather);
+        }
     )
+
+function printWeather(weather) {
+    let tempElement = document.getElementById('tempEl')
+    tempElement.innerText = weather.main.temp;
+}
+
+
+
+
+
+
+
 
