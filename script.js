9430ab54d8e926a809125adf790c8f571fd03e31
Lina155 <AlinfDEmaGitHub2021>
2021-03-04 14:45:48 +0200

    TwoFetch

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"
 // async function getResponse(){
 //     let response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Vinnytsia&appid=b124ec8d5546495f87d1cfc27038a5cc')
 //     let content = await response.json()
@@ -23,6 +24,46 @@
         }
     )
 
+// function Convert(weather){
+//     if (btn.textContent = 'Fahrenheit') {
+//         btn.textContent = 'Celsius'
+//         weather.tempo = ((weather.main.temp - 32) * 5) / 9;
+//         let tempElement = document.getElementById('tempEl')
+//         tempElement.innerText = weather.tempo;
+//     } else if (btn.textContent = 'Celsius') {
+//         btn.textContent = 'Fahrenheit'
+//     }
+// }
+
+function toggleDiv() {
+    let tempDiv = $('#tempEl');
+    //weather.main.temp;
+    let tempoDiv = $('#tempoEl');
+    // (((weather.main.temp - 32) * 5) / 9);
+    tempDiv.toggle();
+    tempoDiv.toggle();
+}
+
+
+
+//
+// btn = document.createElement('BUTTON');
+// btn.id='cm3'; //for css
+// // btn.onclick = function Convert(weather){
+// //     if (btn.textContent = 'Fahrenheit') {
+// //         btn.textContent = 'Celsius'
+// //         weather.temp = ((weather.main.temp - 32) * 5) / 9;
+// //         let tempElement = document.getElementById('tempEl')
+// //         tempElement.innerText = weather.temp;
+// //     } else if (btn.textContent = 'Celsius') {
+// //         btn.textContent = 'Fahrenheit'
+// //     }
+// // }
+// // btn.textContent='Fahrenheit';
+// //
+// document.body.appendChild(btn);//show button
+
+
 function printWeather(weather) {
     let nameElement = document.getElementById('nameEl')
     nameElement.innerText = weather.name;
@@ -35,10 +76,28 @@ function printWeather(weather) {
     //
     let tempElement = document.getElementById('tempEl')
     tempElement.innerText = weather.main.temp;
+
 }
 
+// function Celsius(weather){
+//     document.getElementById('tempEl').append(weather.main.temp);
+// }
 
 
+// function timeEl() {
+//     const cTemp = document.getElementById("timeEL").value;
+//     const cToFahr = cTemp * 9 / 5 + 32;
+//     const massage = cTemp + '°C is ' + cToFahr + '°F.';
+//     document.getElementById("tempEl").innerText = weather.main.temp;
+// }
+//
+// function timeEl() {
+//     const ftemp = document.getElementById("FseC").value;
+//     const fToC = (ftemp - 32) * 5 / 9;
+//     const message = ftemp + '°F is ' + fToC + '°F.';
+//     document.getElementById("tempEl").innerText = weather.main.temp;
+// }
+//
 
 
 
